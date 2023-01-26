8248ed0677a8dd94dddf73344ad2ecc56cd065fe
Md Ahmed Khan <alifftw@gmail.com>
2023-01-25 21:06:26 -0500

    Finished for now

--- Store Manager/main.py
+++ Store Manager/main.py
@@ -1,5 +1,17 @@
-class Item:
-    pass
+from items import Item
+from phone import Phone
+    
+if __name__ == "__main__":
+    #Printing Stuff
 
-item1 = Item()
-item1.name = " Phone"
+    #Item.read_from_csv()
+    #print(Item.whole_list)
+
+
+    phone1 = Phone("jscPhonevi10", 500, 5, 1)
+    #phone1.__name = 'Test'
+    print(Phone.whole_list)
+    #phone1.price = 600
+    print(phone1.price)
+    phone1.increment_price(0.46)
+    print(phone1.price)
