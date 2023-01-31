8bbe2bbad4ac2129d0e721978df779ee060de819
Md Ahmed Khan <alifftw@gmail.com>
2023-01-30 22:24:57 -0500

    Updated lists and stack

--- DSA/Trees/main.py
+++ DSA/Trees/main.py
@@ -1,8 +1,17 @@
-from Tree import Tree
+from ListTree import Tree
 
 tree = Tree('Drinks', [])
+
 cold = Tree('Cold', [])
 hot = Tree('Hot', [])
 
-print(tree)
+test = Tree('test', [])
+fanta = Tree('Fanta', [test])
+
+
 tree.addChild(cold)
+tree.addChild(hot)
+cold.addChild(fanta)
+
+print(tree)
+
