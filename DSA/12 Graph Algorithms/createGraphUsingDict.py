9ba06aa3d6bbe2bf8898546388401ff0e88fdbef
Md Ahmed Khan <alifftw@gmail.com>
2023-12-26 21:12:20 -0500

    update

--- DSA/12 Graph Algorithms/createGraphUsingDict.py
+++ DSA/12 Graph Algorithms/createGraphUsingDict.py
@@ -29,6 +29,18 @@ class Graph:
                     visited.append(others)
                     stack.append(others)
 
+    def newDFS(self, vertex):
+        visited = set()
+        stack = [vertex]
+        while stack:
+            popped = stack.pop()
+            print(popped)
+            for others in self.graphDict[popped]:
+                if others not in visited:
+                    visited.add(others)
+                    stack.append(others)
+
+
 customDict = {
     "a": ["b", "c"],
     "b": ["a", "d", "e"],
@@ -41,4 +53,9 @@ customDict = {
 custom = Graph(customDict)
 custom.addEdge("e", "c")
 
+print('DFS')
 custom.depthFirstSearch("a")
+# print('BFS')
+# custom.breadthFirstSearch("a")
+print("New dfs")
+custom.newDFS('a')
