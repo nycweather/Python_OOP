25abe2fd8cec77a30f9af73e5d9289d4937301d6
Md Ahmed Khan <alifftw@gmail.com>
2023-12-27 22:08:30 -0500

    update

--- DSA/18 Divide & Conquer/houseRobber.py
+++ DSA/18 Divide & Conquer/houseRobber.py
@@ -6,5 +6,26 @@ def rob(n):
         second = rob(n[i+1:])
         return max(first, second)
 
-houses = [6,7,1,30,8,2]
-print(rob(houses))
+
+def rob_DP(nums):
+    if not nums:
+        return 0
+    if len(nums) <= 2:
+        return max(nums)
+
+    dp = [0] * len(nums)
+    dp[0] = nums[0]
+    dp[1] = max(nums[0], nums[1])
+
+    for i in range(2, len(nums)):
+        dp[i] = max(dp[i-1], dp[i-2] + nums[i])
+
+    return dp[-1]
+
+
+# Example usage
+print(rob_DP([6, 9, 3, 1, 2, 4]))  # Output: 4
+
+
+# houses = [6,7,1,30,8,2]
+# print(rob(houses))
